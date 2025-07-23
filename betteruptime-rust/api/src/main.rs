0c7061373dbef156eec0a42de0fc6c196063cc82
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2025-07-23 21:37:57 +0000

    chore: remove unused SVG files and configuration files
    - Deleted SVG files: next.svg, turborepo-dark.svg, turborepo-light.svg, vercel.svg, window.svg.
    - Removed TypeScript configuration files: tsconfig.json from web app and various TypeScript config packages.
    - Deleted package.json files from the root and eslint-config package.
    - Removed UI components: Button, Card, Code, and their respective TypeScript configuration.
    - Cleaned up turbo.json configuration file.

---
+++ betteruptime-rust/api/src/main.rs
@@ -0,0 +1,26 @@
+use std::sync::{Arc, Mutex};
+
+use poem::{
+    get, listener::TcpListener, post, EndpointExt, Route, Server
+};
+use routes::{user::{sign_in, sign_up}, website::{create_website, get_website}};
+use store::store::Store;
+pub mod request_inputs;
+pub mod request_outputs;
+pub mod routes;
+
+#[tokio::main(flavor = "multi_thread")]
+async fn main() -> Result<(), std::io::Error> {
+    let s = Arc::new(Mutex::new(Store::new().unwrap()));
+    let app = Route::new()
+        .at("/website/:website_id", get(get_website))
+        .at("/website", post(create_website))
+        .at("/user/signup", post(sign_up))
+        .at("/user/signin", post(sign_in))
+        .data(s);
+    // creates and runs the http server
+    Server::new(TcpListener::bind("0.0.0.0:3000"))
+        .name("hello-world")
+        .run(app)
+        .await
+}
